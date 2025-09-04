9361afb2ae44ef234c20e04b2beac6dbae33741e
Marzieh Mahmoodi <66200529+0xmarziehlabs@users.noreply.github.com>
2025-09-04 17:37:38 +0330

    Add Semgrep rule and update app/tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,13 @@
-from src.app import hello
+from src.app import hello, insecure_eval
 
 def test_hello():
     assert hello("Tester") == "Hello, Tester!"
 
+def test_insecure_eval_demo():
+    # For demo purposes only. Using a benign expression:
+    assert insecure_eval("1 + 1") == 2
+
 if __name__ == "__main__":
     test_hello()
-    print("Test passed!")
+    test_insecure_eval_demo()
+    print("Tests passed!")
